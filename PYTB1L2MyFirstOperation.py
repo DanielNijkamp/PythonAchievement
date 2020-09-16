32eb642e72fe7140c722310c28e619313c445619
Daniel <danielnijkamp2004@gmail.com>
2020-09-16 15:36:38 +0200

    PYTB1L2MyFirstOperation DNSD1DB

---
+++ PYTB1L2MyFirstOperation.py
@@ -0,0 +1,24 @@
+X= 6
+Y= 4
+Z= 7
+A= 10
+print("--------")
+print(X)
+print("--------")
+print(Y)
+print("--------")
+print(X + Y)
+print("--------")
+print(X - Y)
+print("--------")
+print(X / Y)
+print("--------")
+print(X % Y)
+print("--------")
+X += Y
+print(X)
+print("--------")
+X += Z
+print(X)
+print("--------")
+print(X * A)
