5f5a469c7acfb30d72ccadda48d8411a715c43a4
Daniel <danielnijkamp2004@gmail.com>
2020-09-16 14:15:17 +0200

    PYT:B1:L2 -Poem Printing DNSD1DB

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,6 @@
+vers1 = "Ik ben Daniel ik ben 16 jaar oud en ik woon in Amsterdam, ik vind het leuk om te gamen en zwemmen."
+vers2 = "Ik heb een hekel aan de dam en rennen."
+vers3 = "De games die ik het meests speel is Destiny 2 en Garry's mod."
+vers4 = "In me vrije tijd ga ik soms naar buiten met vrienden of ga ik thuis de pot op"
+
+print(vers1,vers2,vers3,vers4)
