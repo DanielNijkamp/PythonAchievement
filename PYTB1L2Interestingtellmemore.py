22df28672f0cd602e70f6b35703d19af38542cf2
Daniel <danielnijkamp2004@gmail.com>
2020-09-23 15:31:21 +0200

    PYT:B1:L2 Interesting Tell Me More DNSD1Db

---
+++ PYTB1L2Interestingtellmemore.py
@@ -0,0 +1,18 @@
+print("wat is jouw naam?")
+
+naam = input()
+
+print("Aangenaam kennis te maken" + naam + "!")
+
+print("hoe oud ben je?")
+
+leeftijd = input()
+
+print("Dus je heet " + naam + "je bent " + leeftijd + " jaar oud?")
+
+print("Waar woon je?")
+
+plaats = input()
+
+print("ok dus je heet " + naam + " je bent " + leeftijd + " jaar oud en je woont in " + plaats + "?")
+
