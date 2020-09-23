31a8f8be6d06e3eddbb59796d0c0b812b99d653e
Daniel <danielnijkamp2004@gmail.com>
2020-09-23 14:02:00 +0200

    PYT:B1:L2 My First Operation

--- PYTB1L2MyFirstOperation.py
+++ PYTB1L2MyFirstOperation.py
@@ -2,6 +2,8 @@ X= 6
 Y= 4
 Z= 7
 A= 10
+B= 5
+C= 11
 print("--------")
 print(X)
 print("--------")
@@ -18,7 +20,16 @@ print("--------")
 X += Y
 print(X)
 print("--------")
-X += Z
+X /= Z
 print(X)
 print("--------")
 print(X * A)
+print("--------")
+X -= A
+print(X)
+print("--------")
+X *= B
+print(X)
+print("--------")
+X %= C
+print(X)
