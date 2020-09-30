dc7178b121ef933473c7677f583d1005b681dbcb
Daniel <danielnijkamp2004@gmail.com>
2020-09-30 14:17:01 +0200

    PYT:B1:L2 Family Ties DNSD1Db

---
+++ PYTB1L2Familyties.py
@@ -0,0 +1,12 @@
+naam = "Daniel"
+leeftijd = 16
+woonplaats = "Amsterdam"
+afstandSchool = 3.7
+familie1 = "moeder"
+familie2= "broer"
+
+
+print("ik ben " + naam + " ik ben " + str(leeftijd) + " jaar oud")
+print("ik woon in " + woonplaats )
+print("de afstand van mijn huis tot school is " + str(afstandSchool) + " Km")
+print("ik woon thuis met mijn " + familie1 + " en " + familie2)
