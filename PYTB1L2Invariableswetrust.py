ab346aa0cbb6c3be8d0e9b04fe7b5cdd9afb7be6
Daniel <danielnijkamp2004@gmail.com>
2020-10-07 14:28:04 +0200

    PYT:B1:L2 In Variables we trust DNSD1Db

---
+++ PYTB1L2Invariableswetrust.py
@@ -0,0 +1,33 @@
+karakter1= '^'
+float = 5.5
+kebab = True
+shoarma = False
+
+int1 = int(20)
+int2 = int(15)
+
+string1 = str("kebab")
+string2 = str("spies")
+
+
+print("Charachter 1 heeft een karakter waarde, de waarde is: " + karakter1)
+print("-------------------------------------------------------------------")
+print("Float 1 heeft een float-getal waarde, de waarde is: " + str(float))
+print("-------------------------------------------------------------------")
+print("Boolean 1 heeft een ja/nee waarde, de waarde is: " + str(kebab))
+print("Boolean 2 heeft een ja/nee waarde, de waarde is: " + str(shoarma))
+print("-------------------------------------------------------------------")
+print("Integer 1 heeft een getallenwaarde, de waarde is: " + str(int1))
+print("Integer 2 heeft een getallenwaarde, de waarde is: " + str(int2))
+
+int3 = int1 - int2
+
+print("Integer 3 heeft een getallenwaarde, de waarde is: " + str(int3))
+print("-------------------------------------------------------------------")
+
+print("string 1 heeft een tekstwaarde, de waarde is: " + string1 )
+print("string 2 heeft een tekstwaarde, de waarde is: " + string2 )
+
+string3 = string1 + string2
+
+print("string 3 heeft een tekstwaarde, de waarde is: " + string3 ) 
