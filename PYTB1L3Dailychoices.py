2ca4f80093f0ecfc7ab69cd9162cb600c6013315
Daniel <danielnijkamp2004@gmail.com>
2020-10-07 15:23:48 +0200

    PYT:B1:L3 Daily Choices DNSD1Db

---
+++ PYTB1L3Dailychoices.py
@@ -0,0 +1,65 @@
+print("Vraag 1: is trump president?")
+
+antwoord= input()
+
+if antwoord == "ja":
+    print("goed")
+
+if antwoord == "nee":
+    print("fout")
+
+
+print("----------------------------------------------------")
+
+print("Vraag 2: snooz je je telefoon of sta je gelijk op")
+print("typ [sta op] of [snooz]")
+
+antwoord2 = input()
+
+if antwoord2 == "snooz":
+    print("lekker slapen")
+
+if antwoord2 == "sta op":
+    print("vroege vogel")
+
+
+print("----------------------------------------------------")
+
+print("Vraag 3: Ga je met het openbaar vervoer of ga je met eigen vervoer")
+print("typ [ov] of [eigen]")
+
+antwoord3 = input()
+
+if antwoord3 == "ov":
+    print("met de bus of tram leuk")
+
+if antwoord3 == "eigen":
+    print("lekker met de fiets")
+
+print("----------------------------------------------------")
+
+print("vraag 4: kebab of shoarma?")
+print("type [kebab] of [shoarma]")
+
+antwoord4 = input()
+
+if antwoord4 == "kebab":
+    print("lekker kebab")
+
+if antwoord4 == "shoarma":
+    print("lekker shoarma")
+
+print("---------------------------------------------------------")
+
+print("zit je vooraan in de klas of achteraan?")
+print("typ [voor] of [achter]")
+
+antwoord5 = input()
+
+if antwoord5 == "voor":
+    print("voorin lekker man")
+
+if antwoord5 == "achter":
+    print("achterin lekker man")
+
+print("-----------------------------------------------------------")
