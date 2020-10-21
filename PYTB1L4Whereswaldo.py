bc906a0053900fd843fd723463cbf7b90b6a66c3
Daniel <danielnijkamp2004@gmail.com>
2020-10-21 14:29:51 +0200

    PYT:B1:L4 Where's Waldo? DNSD1Db

---
+++ PYTB1L4Whereswaldo.py
@@ -0,0 +1,8 @@
+namenlijst = ("henk", "jan", "dave", "daniel", "alex", "waldo", "peter", "john")
+
+personen = 8
+
+targetPerson = ("waldo")
+
+x = namenlijst.index("waldo")
+print("Waldo zat op stoel nummer: " , x)
